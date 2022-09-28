4ff192cf30a4918ecfb907dada716f0ed63ea8ab
Mixu <mikael@softagram.com>
2022-09-28 13:42:28 +0300

    Hmm

--- api/src/middleware/authentication.ts
+++ api/src/middleware/authentication.ts
@@ -9,10 +9,6 @@ export const authenticateUser = catchErrors(async (req, _res, next) => {
   if (!token) {
     throw new InvalidTokenError('Authentication token not found.');
   }
-  const userId = verifyToken(token).sub;
-  if (!userId) {
-    throw new InvalidTokenError('Authentication token is invalid.');
-  }
   const user = await User.findOne(userId);
   if (!user) {
     throw new InvalidTokenError('Authentication token is invalid: User not found.');
